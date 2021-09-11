4c119b6b034c31c68035c6934e9d90cc3a2969e9
elkhoudiry <ahmedelkhodery2@Gmail.com>
2021-09-11 12:11:25 +0200

    add basic routes to client app

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { Join, Chat } from "./components";
 
 const App = () => {
     return (
-        <div>
-            <h1>HELLO!</h1>
-        </div>
+        <Router>
+            <Route path="/" exact component={Join} />
+            <Route path="/chat"  component={ Chat }/>
+
+       </Router>
     )
 }
 
---
+++ packages/client/src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Join } from "./join/join";
+export { default as Chat } from "./chat/chat";
