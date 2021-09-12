155772fd3aad1359d5b6f7e88b50480aa4636b11
elkhoudiry <ahmedelkhodery2@Gmail.com>
2021-09-12 16:42:21 +0200

    refactor: adding basic chat component

--- packages/client/src/components/chat/chat.tsx
+++ packages/client/src/components/chat/chat.tsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
+import { Location } from "history"
+import queryString from "query-string"
+import io, { Socket } from "socket.io-client"
+import dotenv from "dotenv"
 
-const Chat = () => {
+import "./chat.css"
+
+dotenv.config()
+
+let socket: Socket;
+
+const Chat = ({ location }: { location: Location }) => {
+
+    const [clientName, setName] = useState("")
+    const [clientEmail, setEmail] = useState("")
+    const ENDPOINT = process.env.REACT_APP_ENDPOINT || ""
+
+    useEffect(() => {
+        const { name, email } = queryString.parse(location.search)
+
+        socket = io(ENDPOINT)
+
+        name && setName(Array.isArray(name) ? name[0] : name)
+        email && setEmail(Array.isArray(email) ? email[0] : email)
+
+        socket.emit('join', { clientName, clientEmail })
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [ENDPOINT, location.search])
+
     return (
         <h1>Chat !</h1>
     )
