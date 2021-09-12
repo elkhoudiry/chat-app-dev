00df3ca357e7f83e31314983a714256e2eb54f59
elkhoudiry <ahmedelkhodery2@Gmail.com>
2021-09-12 15:19:14 +0200

    refactor: adding basic join compenent

--- packages/client/src/components/join/join.tsx
+++ packages/client/src/components/join/join.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+
+import "./join.css"
 
 const Join = () => {
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
     return (
-        <h1>JOIN !</h1>
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
+                <div><input placeholder="Email" className="joinInput mt-20" type="text" onChange={(event) => setEmail(event.target.value)} /></div>
+                <Link onClick={event => (!name || !email) ? event.preventDefault() : null} to={`/chat?name=${name}&email=${email}`}>
+                    <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
