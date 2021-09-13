cc73a39df1583cf0f51e004b650b29cdc75d9e4b
elkhoudiry <ahmedelkhodery2@Gmail.com>
2021-09-13 17:42:44 +0200

    add some fake users to test joining with

--- packages/client/src/components/join/join.tsx
+++ packages/client/src/components/join/join.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
 import "./join.css"
@@ -8,12 +8,23 @@ const Join = () => {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
 
+    useEffect(() => {
+        if (process.env.REACT_APP_LOCAL && !name) {
+            import("./test_data").then((module: { users: Array<any> }) => {
+                const user = module.users[Math.floor(Math.random() * 200)];
+                setName(`${user.first_name} ${user.last_name}`)
+                setEmail(user.email)
+            })
+        }
+        return () => { }
+    }, [name, email])
+
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
                 <h1 className="heading">Join</h1>
-                <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
-                <div><input placeholder="Email" className="joinInput mt-20" type="text" onChange={(event) => setEmail(event.target.value)} /></div>
+                <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} value={name} /></div>
+                <div><input placeholder="Email" className="joinInput mt-20" type="text" onChange={(event) => setEmail(event.target.value)} value={email} /></div>
                 <Link onClick={event => (!name || !email) ? event.preventDefault() : null} to={`/chat?name=${name}&email=${email}`}>
                     <button className="button mt-20" type="submit">Sign In</button>
                 </Link>
