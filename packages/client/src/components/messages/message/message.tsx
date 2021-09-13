3ad62d3dbcb2a4e90d2532cb80084b87a978a9ea
elkhoudiry <ahmedelkhodery2@Gmail.com>
2021-09-14 00:02:54 +0200

    change to vertical chat message component

--- packages/client/src/components/messages/message/message.tsx
+++ packages/client/src/components/messages/message/message.tsx
@@ -16,19 +16,19 @@ const Message = ({ message, name }: { message: ChatMessage, name: string }) => {
     return (
         isSentByCurrentUser
             ? (
-                <div className="messageContainer justifyEnd">
-                    <p className="sentText pr-10">{name}</p>
+                <div className="messageContainer">
+                    <p className="sentText">{name}</p>
                     <div className="messageBox backgroundBlue">
                         <p className="messageText colorWhite">{ReactEmoji.emojify(message.message)}</p>
                     </div>
                 </div>
             )
             : (
-                <div className="messageContainer justifyStart">
+                <div className="messageContainer">
+                    <p className="sentText">{message.sender}</p>
                     <div className="messageBox backgroundLight">
                         <p className="messageText colorDark">{ReactEmoji.emojify(message.message)}</p>
                     </div>
-                    <p className="sentText pl-10 ">{message.sender}</p>
                 </div>
             )
     );
