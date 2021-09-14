f85dc13e93ab38cbb6af5a7469177d900abde897
elkhoudiry <ahmedelkhodery2@Gmail.com>
2021-09-14 21:59:56 +0200

    add email code verification on client side

--- packages/client/src/components/join/join.tsx
+++ packages/client/src/components/join/join.tsx
@@ -1,16 +1,46 @@
 import React, { useState, useEffect } from "react"
+import { useHistory } from 'react-router-dom'
 import { EmailCode } from '../index'
+import io, { Socket } from 'socket.io-client'
 import "./join.css"
+
+const ENDPOINT = process.env.REACT_APP_ENDPOINT ?? "localhost:5430"
+let socket: Socket;
 
 const Join = () => {
 
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [showEmailCode, setShowEmailCode] = useState(false)
+    const history = useHistory()
 
     useEffect(() => {
-        return () => { }
-    }, [name, email])
+        socket = io(ENDPOINT)
+
+        return () => {
+            socket.disconnect()
+        }
+    }, [])
+
+    const newConnection = () => {
+        socket.emit("new-connection", ({ email, name }), ({ error }: { error: string | undefined }) => {
+            if (error && error === "need to verify email") {
+                setShowEmailCode(() => true)
+            } else {
+                history.push(`/chat?name=${name}&email=${email}`)
+            }
+        })
+    }
+
+    const validateEmail = (email: string) => {
+        var mailformat = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/
+        if (email.match(mailformat)) {
+            newConnection()
+        }
+        else {
+            alert("You have entered an invalid email address!");
+        }
+    }
 
     return (
         <div className="joinOuterContainer">
@@ -18,8 +48,8 @@ const Join = () => {
                 <h1 className="heading">Contact Ahmed Elkhoudiry</h1>
                 <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} value={name} /></div>
                 <div><input placeholder="Email" className="joinInput mt-20" type="text" onChange={(event) => setEmail(event.target.value)} value={email} /></div>
-                <button className="button mt-20" type="submit" onClick={() => setShowEmailCode(() => true)}>Send Code</button>
-                <EmailCode name={name} email={email} showEmailCode={showEmailCode} setShowEmailCode={setShowEmailCode} />
+                <button className="button mt-20" type="submit" onClick={event => (!name || !email) ? event.preventDefault() : validateEmail(email)}>Send Code</button>
+                <EmailCode socket={socket} name={name} email={email} showEmailCode={showEmailCode} setShowEmailCode={setShowEmailCode} />
             </div>
         </div>
     )
--- packages/client/src/components/verfication/email-code.tsx
+++ packages/client/src/components/verfication/email-code.tsx
@@ -1,14 +1,26 @@
 import React, { useState } from 'react'
 import Modal from 'react-modal'
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import { Socket } from 'socket.io-client'
 
 import "./email-code.css"
 
 Modal.setAppElement("#root")
 
-const EmailCode = ({ name, email, showEmailCode, setShowEmailCode }: { name: string, email: string, showEmailCode: boolean, setShowEmailCode: Function }) => {
+const EmailCode = ({ socket, name, email, showEmailCode, setShowEmailCode }: { socket: Socket, name: string, email: string, showEmailCode: boolean, setShowEmailCode: Function }) => {
 
-    const [emailCode, setEmailCode] = useState("")
+    const [code, setEmailCode] = useState("")
+    const history = useHistory()
+    const verifyCode = () => {
+        socket.emit("verify-code", ({ email, code }), ({ success }: { success: boolean }) => {
+            if (success) {
+                setShowEmailCode(() => false)
+                history.push(`/chat?name=${name}&email=${email}`)
+            } else {
+                alert("failed to verify")
+            }
+        })
+    }
 
     return (
         <Modal isOpen={showEmailCode} className="emailCardModalContentStyle" overlayClassName="emailCardModalOverlayStyle">
@@ -17,10 +29,8 @@ const EmailCode = ({ name, email, showEmailCode, setShowEmailCode }: { name: str
                     <h1 className="heading">Email Code Verfication</h1>
                     <p className="emailCodeBodyParagraph">An Email Message Has Been Sent to {email} With a Verfication Code, Enter the code that is sent to your email</p>
                     <p className="emailCodeBodyParagraph">Remember to check your junk or spam folders</p>
-                    <div><input placeholder="Code" className="joinInput" type="text" onChange={(event) => setEmailCode(event.target.value)} value={emailCode} /></div>
-                    <Link onClick={event => (!name || !email) ? event.preventDefault() : null} to={`/chat?name=${name}&email=${email}`}>
-                        <button className="button mt-20" type="submit">Join</button>
-                    </Link>
+                    <div><input placeholder="Code" className="joinInput" type="text" onChange={(event) => setEmailCode(event.target.value)} value={code} /></div>
+                    <button onClick={event => (!code || code.length !== 10) ? event.preventDefault() : verifyCode()} className="button mt-20" type="submit">Join</button>
                 </div>
             </div>
         </Modal>
